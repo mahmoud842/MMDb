5aa382f7f725f01e67637903fc3053cc04eef32b
mahmoud842 <mahmoud1hesham2003@gmail.com>
2023-06-29 14:56:54 +0300

    add films.csv and represented it in a table in index.html template

--- MMDb/filmsList/views.py
+++ MMDb/filmsList/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from datetime import datetime
+import csv
 
 def index(request):
-    date = datetime.now()
-    return render(request, "filmsList/index.html",{
-        'year':date.year,
-        'month':date.month,
-        'day':date.day
-    })
+
+    with open('films.csv', newline='') as filmscsv:
+        films_dict = csv.DictReader(filmscsv)
+        date = datetime.now()
+        return render(request, "filmsList/index.html",{
+            'year':date.year,
+            'month':date.month,
+            'day':date.day,
+            'films':films_dict
+        })
