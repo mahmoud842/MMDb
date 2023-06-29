3a6b3cc39b9cf8daeb2c655096e796c02a737a8e
mahmoud842 <mahmoud1hesham2003@gmail.com>
2023-06-29 03:52:28 +0300

    created html file and styled it & put the date in the upper write corner of the page

--- MMDb/filmsList/views.py
+++ MMDb/filmsList/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from datetime import datetime
+
+def index(request):
+    date = datetime.now()
+    return render(request, "filmsList/index.html",{
+        'year':date.year,
+        'month':date.month,
+        'day':date.day
+    })
